58400cccd49f6ae8956b52754ccf4943ad2fcb1b
RealDzolat <dzolbusines@gmail.com>
2026-07-04 10:55:00 +0200

    Added Actions

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 #include <Windows.h>
 #include "classes/customer.hpp"
 #include "functions/input.hpp"
 #include "functions/random.hpp"
 #include "functions/config.hpp"
+
+std::map<char, bool> validActions{{'c', true}, {'e', true}};
 
 void Startup()
 {
@@ -21,7 +24,42 @@ void Startup()
     std::cout << "Welcome, " << cf.name << "\n";
 }
 
+namespace Action
+{
+    char Choose()
+    {
+        std::cout << "[c]\tCustomers\n";
+        std::cout << "[e]\tExit\n";
+        while (true)
+        {
+            char action{get_input<char>("Please enter your action:")};
+
+            if (validActions.contains(action))
+                return action;
+        }
+    }
+
+    void Act(const char action)
+    {
+        switch (action)
+        {
+        case 'c':
+            /* code */
+            break;
+        case 'e':
+            abort();
+            break;
+        default:
+            break;
+        }
+    }
+
+}
+
 int main()
 {
     Startup();
+    ConfigFile cf{Config::Read()};
+
+    Action::Act(Action::Choose());
 }
